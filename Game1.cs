895b92b78824d724ec15128dcf16652288c3a916
railks21 <josepedro.abreu21@gmail.com>
2024-05-07 13:29:01 +0100

    Ajuste do ecra e menu

--- Game1.cs
+++ Game1.cs
@@ -2,12 +2,21 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using System;
+
 namespace TDJ_PJ2
 {
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        #region Utility variables
+        public static int ScreenWidth;
+        public static int ScreenHeight;
+        #endregion
+
+        public SceneManager Scenes;
 
         public Game1()
         {
@@ -19,10 +28,15 @@ namespace TDJ_PJ2
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            
+
+            // Utility variables init
+            ScreenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            ScreenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
             // Changing the game window size
-            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
-            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            Window.Position = Point.Zero;
+            _graphics.PreferredBackBufferWidth = ScreenWidth;
+            _graphics.PreferredBackBufferHeight = ScreenHeight;
             
             _graphics.ApplyChanges();
 
@@ -32,6 +46,10 @@ namespace TDJ_PJ2
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            AssetManager.Instance().LoadAssets(Content);
+
+            Scenes = new SceneManager();
 
             // TODO: use this.Content to load your game content here
         }
@@ -43,16 +61,37 @@ namespace TDJ_PJ2
 
             // TODO: Add your update logic here
 
+            Scenes.Update(gameTime);
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
+            // Rendering stuff here
+            _spriteBatch.Begin();
+
+            #region Managers render
+            // Tiles render
+            //Tiles.Render(_spriteBatch);
+
+            // Scenes render
+            Scenes.Render(_spriteBatch);
+            #endregion
+
+            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+        // A util function that will center a text based on its font and contents
+        public static Vector2 CenterText(SpriteFont font, string text)
+        {
+            return new Vector2(ScreenWidth / 2 - font.MeasureString(text).X / 2,
+                               ScreenHeight / 4 - font.MeasureString(text).Y / 4);
         }
     }
 }
---
+++ src/Managers/SceneManager.cs
@@ -0,0 +1,87 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TDJ_PJ2;
+
+public class SceneManager
+{
+    #region Delegate
+    public delegate void SceneChange(SceneType sceneType);
+    #endregion
+
+    #region Fields
+    //public ScoreManager Score;
+    public SceneType Type {get; set;}
+    public IScene CurrentScene;
+    private bool m_IsSceneChanged;
+    #endregion
+
+    #region Constructor
+    public SceneManager()
+    {
+        //Score = new ScoreManager();
+
+        Type = SceneType.Menu;
+        CurrentScene = new MainMenuScene();
+
+        m_IsSceneChanged = false;
+
+        // Subscribing to events
+        //MainMenuScene.SceneChangeEvent += OnSceneChange;
+        //GameScene.SceneChangeEvent += OnSceneChange;
+        //HelpScene.SceneChangeEvent += OnSceneChange;
+        //CreditsScene.SceneChangeEvent += OnSceneChange;
+        //OverScene.SceneChangeEvent += OnSceneChange;
+        //EntityManager.SceneChangeEvent += OnSceneChange;
+    }
+    #endregion
+
+    #region Methods
+    public void Update(GameTime gameTime)
+    {
+        // Only updating the current scene
+        CurrentScene.Update(gameTime);
+
+        // Only loading the scene when there is a change in the state
+        if(!m_IsSceneChanged) return;
+
+        switch(Type)
+        {
+            case SceneType.Menu:
+                CurrentScene = new MainMenuScene();
+                break;
+            //case SceneType.Game:
+            //    CurrentScene = new GameScene(Score);
+            //    break;
+            //case SceneType.Help:
+            //    CurrentScene = new HelpScene();
+            //    break;
+            //case SceneType.Credits:
+            //    CurrentScene = new CreditsScene();
+            //    break;
+            //case SceneType.Over:
+            //    CurrentScene = new OverScene(Score);
+            //    break;
+        }
+
+        // Making sure that the scene loads once not every frame
+        m_IsSceneChanged = false;
+    }
+    
+    public void Render(SpriteBatch spriteBatch)
+    {
+        // Only rendering the current scene
+        CurrentScene.Render(spriteBatch);
+    }
+
+    //public void OnSceneChange(SceneType sceneType)
+    //{
+    //    Type = sceneType;
+
+    //    // Resetting the score when the game is replayed
+    //    if (Type == SceneType.Game);
+
+    //    m_IsSceneChanged = true;
+    //}
+    #endregion
+}
